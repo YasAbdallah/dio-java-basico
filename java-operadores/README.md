aeedf492c12b0a528c33f8e1bfacd1362d860bfe
YasAbdallah <yassercondoluci@hotmail.com>
2024-05-10 16:39:37 -0400

    Aprendendo sobre operadores

---
+++ java-operadores/README.md
@@ -0,0 +1,7 @@
+# Operadores
+
+Aprendendo sobre operadores aritiméticos;
+
+## Mais
+
+Para mais detalhes sobre operadores em Java [Clique aqui](https://glysns.gitbook.io/java-basico/sintaxe/operadores) para acessar o Gitbook.
---
+++ java-operadores/src/App.java
@@ -0,0 +1,29 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //Operador de atribuição =
+        String nome = "Yasser";
+        System.out.println(nome);
+        int idade = 27;
+        System.out.println(idade);
+
+        //Operadores aritméticos
+        // + (adição ou concatenação entre variaveis)
+        double soma = 10.5 + 15.7;
+        System.out.println(soma);
+        // - (subtração)
+        int subtracao = 113 - 25;
+        System.out.println(subtracao);
+        // * (multiplicação)
+        int multiplicacao = 20 * 7;
+        System.out.println(multiplicacao);
+        // /(divisão)
+        int divisao = 15 / 3;
+        System.out.println(divisao);
+        // % (modulo ou resto da divisão)
+        int modulo = 18 % 3;
+        System.out.println(modulo);
+        // () (usado para afirmar a orden de precedencia.) 
+        double resultado = (10 * 7) + (20 / 4);
+        System.out.println(resultado);
+    }
+}
