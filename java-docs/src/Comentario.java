3c54824afd5df30bfcf815ac5bb827448376f3c7
YasAbdallah <yassercondoluci@hotmail.com>
2024-05-17 14:17:29 -0400

    Um pouco sobre documentação Java.

---
+++ java-docs/src/Comentario.java
@@ -0,0 +1,21 @@
+public class Comentario{
+    public static void main(String[] args) {
+        // Olá, eu sou um comentário de uma única linha
+
+        /*
+         * Olá,
+         * Eu sou um  comentário 
+         * de varias linhas.
+         */
+ 
+    }
+    /**
+ * Estas duas estrelinas acima
+ * é para uidentificar que você pretende  elaborar um cometario
+ * a nivel de documentação.  
+ * Que Legal!!!
+ */
+    public void metodo(){
+
+    }
+}
